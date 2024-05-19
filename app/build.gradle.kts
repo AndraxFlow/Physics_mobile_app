66118d872bf5c0d721c98e8073607688bd56c3f9
Andrax <andronov_maksimka@mail.ru>
2024-05-19 19:18:17 +0300

    Ready to use

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,6 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
-
-    // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
